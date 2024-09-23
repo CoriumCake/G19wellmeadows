c1a41811b64c9a7cdbaa3b51af5c7d38cda183d3
fusetanya <143582986+fusetanya@users.noreply.github.com>
2024-09-23 22:51:16 +0700

    ward

---
+++ wellmeadows/FormWard.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormWard
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.RoundCornerTextbox5 = New wellmeadows.RoundCornerTextbox()
+        Me.RoundCornerTextbox4 = New wellmeadows.RoundCornerTextbox()
+        Me.RoundCornerTextbox3 = New wellmeadows.RoundCornerTextbox()
+        Me.RoundCornerTextbox2 = New wellmeadows.RoundCornerTextbox()
+        Me.RoundCornerTextbox1 = New wellmeadows.RoundCornerTextbox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'RoundCornerTextbox5
+        '
+        Me.RoundCornerTextbox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoundCornerTextbox5.Location = New System.Drawing.Point(454, 106)
+        Me.RoundCornerTextbox5.Margin = New System.Windows.Forms.Padding(2, 2, 2, 5)
+        Me.RoundCornerTextbox5.Name = "RoundCornerTextbox5"
+        Me.RoundCornerTextbox5.Size = New System.Drawing.Size(197, 23)
+        Me.RoundCornerTextbox5.TabIndex = 52
+        '
+        'RoundCornerTextbox4
+        '
+        Me.RoundCornerTextbox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoundCornerTextbox4.Location = New System.Drawing.Point(708, 106)
+        Me.RoundCornerTextbox4.Margin = New System.Windows.Forms.Padding(2, 2, 2, 5)
+        Me.RoundCornerTextbox4.Name = "RoundCornerTextbox4"
+        Me.RoundCornerTextbox4.Size = New System.Drawing.Size(197, 23)
+        Me.RoundCornerTextbox4.TabIndex = 51
+        '
+        'RoundCornerTextbox3
+        '
+        Me.RoundCornerTextbox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoundCornerTextbox3.Location = New System.Drawing.Point(244, 106)
+        Me.RoundCornerTextbox3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 5)
+        Me.RoundCornerTextbox3.Name = "RoundCornerTextbox3"
+        Me.RoundCornerTextbox3.Size = New System.Drawing.Size(197, 23)
+        Me.RoundCornerTextbox3.TabIndex = 50
+        '
+        'RoundCornerTextbox2
+        '
+        Me.RoundCornerTextbox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoundCornerTextbox2.Location = New System.Drawing.Point(32, 196)
+        Me.RoundCornerTextbox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 5)
+        Me.RoundCornerTextbox2.Name = "RoundCornerTextbox2"
+        Me.RoundCornerTextbox2.Size = New System.Drawing.Size(197, 23)
+        Me.RoundCornerTextbox2.TabIndex = 49
+        '
+        'RoundCornerTextbox1
+        '
+        Me.RoundCornerTextbox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoundCornerTextbox1.Location = New System.Drawing.Point(32, 106)
+        Me.RoundCornerTextbox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 5)
+        Me.RoundCornerTextbox1.Name = "RoundCornerTextbox1"
+        Me.RoundCornerTextbox1.Size = New System.Drawing.Size(197, 23)
+        Me.RoundCornerTextbox1.TabIndex = 48
+        '
+        'Label8
+        '
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(29, 163)
+        Me.Label8.Margin = New System.Windows.Forms.Padding(27, 0, 2, 0)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(100, 31)
+        Me.Label8.TabIndex = 47
+        Me.Label8.Text = "Ward Name"
+        Me.Label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label7
+        '
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(714, 62)
+        Me.Label7.Margin = New System.Windows.Forms.Padding(27, 0, 2, 0)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(100, 31)
+        Me.Label7.TabIndex = 46
+        Me.Label7.Text = "Bed number"
+        Me.Label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label5
+        '
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(461, 62)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(27, 0, 2, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(100, 31)
+        Me.Label5.TabIndex = 45
+        Me.Label5.Text = "Ward location"
+        Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(250, 62)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(27, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 31)
+        Me.Label1.TabIndex = 44
+        Me.Label1.Text = "Phone"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(29, 62)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(27, 0, 2, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 31)
+        Me.Label2.TabIndex = 43
+        Me.Label2.Text = "Ward ID"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(-135, 9)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(534, 40)
+        Me.Label3.TabIndex = 42
+        Me.Label3.Text = "Ward Management"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(237, Byte), Integer), CType(CType(237, Byte), Integer), CType(CType(235, Byte), Integer))
+        Me.Button2.BackgroundImage = Global.wellmeadows.My.Resources.Resources.login_button1
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(237, Byte), Integer), CType(CType(237, Byte), Integer), CType(CType(235, Byte), Integer))
+        Me.Button2.Location = New System.Drawing.Point(775, 432)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(2, 5, 2, 6)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(113, 36)
+        Me.Button2.TabIndex = 54
+        Me.Button2.Text = "NEXT"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(237, Byte), Integer), CType(CType(237, Byte), Integer), CType(CType(235, Byte), Integer))
+        Me.Button1.BackgroundImage = Global.wellmeadows.My.Resources.Resources.login_button1
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(237, Byte), Integer), CType(CType(237, Byte), Integer), CType(CType(235, Byte), Integer))
+        Me.Button1.Location = New System.Drawing.Point(637, 432)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 5, 2, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(113, 36)
+        Me.Button1.TabIndex = 53
+        Me.Button1.Text = "ADD"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'FormWard
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(949, 502)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RoundCornerTextbox5)
+        Me.Controls.Add(Me.RoundCornerTextbox4)
+        Me.Controls.Add(Me.RoundCornerTextbox3)
+        Me.Controls.Add(Me.RoundCornerTextbox2)
+        Me.Controls.Add(Me.RoundCornerTextbox1)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label3)
+        Me.Name = "FormWard"
+        Me.Text = "FormWard"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RoundCornerTextbox5 As RoundCornerTextbox
+    Friend WithEvents RoundCornerTextbox4 As RoundCornerTextbox
+    Friend WithEvents RoundCornerTextbox3 As RoundCornerTextbox
+    Friend WithEvents RoundCornerTextbox2 As RoundCornerTextbox
+    Friend WithEvents RoundCornerTextbox1 As RoundCornerTextbox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
