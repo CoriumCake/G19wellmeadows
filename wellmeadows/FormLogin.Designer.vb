2f972772ea8d8a662f7fabb1578d68a5893cfca3
CoriumCake <45147371+CoriumCake@users.noreply.github.com>
2024-09-24 03:04:43 +0700

    update login

--- wellmeadows/FormLogin.Designer.vb
+++ wellmeadows/FormLogin.Designer.vb
@@ -146,7 +146,7 @@ Partial Class Form1
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(181, 62)
         Me.Label3.TabIndex = 0
-        Me.Label3.Text = "Allah"
+        Me.Label3.Text = "Login"
         Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label4
