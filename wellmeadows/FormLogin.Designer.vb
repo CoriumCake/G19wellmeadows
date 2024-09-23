b660fd96333ca30d5365b6a2b14068f4bb441218
CoriumCake <45147371+CoriumCake@users.noreply.github.com>
2024-09-24 01:28:47 +0700

    A

--- wellmeadows/FormLogin.Designer.vb
+++ wellmeadows/FormLogin.Designer.vb
@@ -50,9 +50,8 @@ Partial Class Form1
         Me.Panel1.Controls.Add(Me.Panel2)
         Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.Panel1.Location = New System.Drawing.Point(0, 0)
-        Me.Panel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(556, 365)
+        Me.Panel1.Size = New System.Drawing.Size(834, 562)
         Me.Panel1.TabIndex = 2
         '
         'Panel3
@@ -61,10 +60,9 @@ Partial Class Form1
         Me.Panel3.BackColor = System.Drawing.Color.FromArgb(CType(CType(25, Byte), Integer), CType(CType(23, Byte), Integer), CType(CType(21, Byte), Integer))
         Me.Panel3.Controls.Add(Me.Label1)
         Me.Panel3.Controls.Add(Me.PictureBox1)
-        Me.Panel3.Location = New System.Drawing.Point(2, 0)
-        Me.Panel3.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Panel3.Location = New System.Drawing.Point(3, 0)
         Me.Panel3.Name = "Panel3"
-        Me.Panel3.Size = New System.Drawing.Size(552, 68)
+        Me.Panel3.Size = New System.Drawing.Size(828, 105)
         Me.Panel3.TabIndex = 2
         '
         'Label1
@@ -74,10 +72,9 @@ Partial Class Form1
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.White
-        Me.Label1.Location = New System.Drawing.Point(198, 18)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Location = New System.Drawing.Point(297, 28)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(255, 60)
+        Me.Label1.Size = New System.Drawing.Size(382, 92)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Wellmeadows"
         Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -89,10 +86,9 @@ Partial Class Form1
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.PictureBox1.BackgroundImage = Global.wellmeadows.My.Resources.Resources.logo
         Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.PictureBox1.Location = New System.Drawing.Point(153, 18)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.PictureBox1.Location = New System.Drawing.Point(230, 28)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(49, 49)
+        Me.PictureBox1.Size = New System.Drawing.Size(74, 75)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
@@ -105,9 +101,8 @@ Partial Class Form1
         Me.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.Panel2.Controls.Add(Me.TableLayoutPanel1)
         Me.Panel2.Location = New System.Drawing.Point(0, 0)
-        Me.Panel2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Panel2.Name = "Panel2"
-        Me.Panel2.Size = New System.Drawing.Size(556, 365)
+        Me.Panel2.Size = New System.Drawing.Size(834, 562)
         Me.Panel2.TabIndex = 1
         '
         'TableLayoutPanel1
@@ -130,8 +125,7 @@ Partial Class Form1
         Me.TableLayoutPanel1.Controls.Add(Me.Button1, 1, 4)
         Me.TableLayoutPanel1.Controls.Add(Me.RoundCornerTextbox1, 1, 1)
         Me.TableLayoutPanel1.Controls.Add(Me.RoundCornerTextbox2, 1, 2)
-        Me.TableLayoutPanel1.Location = New System.Drawing.Point(94, 80)
-        Me.TableLayoutPanel1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(141, 123)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 5
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.10582!))
@@ -139,7 +133,7 @@ Partial Class Form1
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 22.22222!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.58201!))
         Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 24.33862!))
-        Me.TableLayoutPanel1.Size = New System.Drawing.Size(383, 257)
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(574, 395)
         Me.TableLayoutPanel1.TabIndex = 3
         '
         'Label3
@@ -148,12 +142,11 @@ Partial Class Form1
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label3.BackColor = System.Drawing.Color.Transparent
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(147, 11)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label3.Location = New System.Drawing.Point(221, 17)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(121, 40)
+        Me.Label3.Size = New System.Drawing.Size(181, 62)
         Me.Label3.TabIndex = 0
-        Me.Label3.Text = "Login"
+        Me.Label3.Text = "Allah"
         Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Label4
@@ -162,10 +155,10 @@ Partial Class Form1
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(27, 108)
-        Me.Label4.Margin = New System.Windows.Forms.Padding(27, 0, 2, 0)
+        Me.Label4.Location = New System.Drawing.Point(40, 167)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(40, 0, 3, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(116, 57)
+        Me.Label4.Size = New System.Drawing.Size(175, 88)
         Me.Label4.TabIndex = 2
         Me.Label4.Text = "Password"
         Me.Label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -176,10 +169,10 @@ Partial Class Form1
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(27, 51)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(27, 0, 2, 0)
+        Me.Label2.Location = New System.Drawing.Point(40, 79)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(40, 0, 3, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(116, 57)
+        Me.Label2.Size = New System.Drawing.Size(175, 88)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Username"
         Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
@@ -188,10 +181,10 @@ Partial Class Form1
         '
         Me.CheckBox1.AutoSize = True
         Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.CheckBox1.Location = New System.Drawing.Point(147, 167)
-        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 5)
+        Me.CheckBox1.Location = New System.Drawing.Point(221, 258)
+        Me.CheckBox1.Margin = New System.Windows.Forms.Padding(3, 3, 3, 8)
         Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(99, 17)
+        Me.CheckBox1.Size = New System.Drawing.Size(144, 24)
         Me.CheckBox1.TabIndex = 3
         Me.CheckBox1.Text = "show password"
         Me.CheckBox1.UseVisualStyleBackColor = True
@@ -202,10 +195,9 @@ Partial Class Form1
         Me.LinkLabel1.Dock = System.Windows.Forms.DockStyle.Top
         Me.LinkLabel1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.LinkLabel1.LinkColor = System.Drawing.Color.Black
-        Me.LinkLabel1.Location = New System.Drawing.Point(272, 165)
-        Me.LinkLabel1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.LinkLabel1.Location = New System.Drawing.Point(408, 255)
         Me.LinkLabel1.Name = "LinkLabel1"
-        Me.LinkLabel1.Size = New System.Drawing.Size(109, 23)
+        Me.LinkLabel1.Size = New System.Drawing.Size(163, 35)
         Me.LinkLabel1.TabIndex = 5
         Me.LinkLabel1.TabStop = True
         Me.LinkLabel1.Text = "forgot password?"
@@ -221,10 +213,10 @@ Partial Class Form1
         Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(237, Byte), Integer), CType(CType(237, Byte), Integer), CType(CType(235, Byte), Integer))
-        Me.Button1.Location = New System.Drawing.Point(151, 197)
-        Me.Button1.Margin = New System.Windows.Forms.Padding(2, 5, 2, 6)
+        Me.Button1.Location = New System.Drawing.Point(226, 305)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 8, 3, 9)
         Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(113, 36)
+        Me.Button1.Size = New System.Drawing.Size(170, 55)
         Me.Button1.TabIndex = 9
         Me.Button1.Text = "Login"
         Me.Button1.UseVisualStyleBackColor = False
@@ -234,10 +226,10 @@ Partial Class Form1
         Me.RoundCornerTextbox1.Anchor = System.Windows.Forms.AnchorStyles.Left
         Me.TableLayoutPanel1.SetColumnSpan(Me.RoundCornerTextbox1, 2)
         Me.RoundCornerTextbox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RoundCornerTextbox1.Location = New System.Drawing.Point(147, 66)
-        Me.RoundCornerTextbox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 5)
+        Me.RoundCornerTextbox1.Location = New System.Drawing.Point(221, 105)
+        Me.RoundCornerTextbox1.Margin = New System.Windows.Forms.Padding(3, 3, 3, 8)
         Me.RoundCornerTextbox1.Name = "RoundCornerTextbox1"
-        Me.RoundCornerTextbox1.Size = New System.Drawing.Size(197, 23)
+        Me.RoundCornerTextbox1.Size = New System.Drawing.Size(294, 30)
         Me.RoundCornerTextbox1.TabIndex = 10
         '
         'RoundCornerTextbox2
@@ -245,23 +237,22 @@ Partial Class Form1
         Me.RoundCornerTextbox2.Anchor = System.Windows.Forms.AnchorStyles.Left
         Me.TableLayoutPanel1.SetColumnSpan(Me.RoundCornerTextbox2, 2)
         Me.RoundCornerTextbox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.RoundCornerTextbox2.Location = New System.Drawing.Point(147, 123)
-        Me.RoundCornerTextbox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 5)
+        Me.RoundCornerTextbox2.Location = New System.Drawing.Point(221, 193)
+        Me.RoundCornerTextbox2.Margin = New System.Windows.Forms.Padding(3, 3, 3, 8)
         Me.RoundCornerTextbox2.Name = "RoundCornerTextbox2"
-        Me.RoundCornerTextbox2.Size = New System.Drawing.Size(197, 23)
+        Me.RoundCornerTextbox2.Size = New System.Drawing.Size(294, 30)
         Me.RoundCornerTextbox2.TabIndex = 11
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(25, Byte), Integer), CType(CType(23, Byte), Integer), CType(CType(21, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(556, 365)
+        Me.ClientSize = New System.Drawing.Size(834, 562)
         Me.Controls.Add(Me.Panel1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
-        Me.MaximumSize = New System.Drawing.Size(670, 528)
-        Me.MinimumSize = New System.Drawing.Size(570, 398)
+        Me.MaximumSize = New System.Drawing.Size(994, 782)
+        Me.MinimumSize = New System.Drawing.Size(844, 582)
         Me.Name = "Form1"
         Me.Text = "Wellsmeadows Hospital"
         Me.Panel1.ResumeLayout(False)
